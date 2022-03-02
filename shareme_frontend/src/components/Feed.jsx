c44847d52316f6c5ef5a9c9606e1b981852959e0
wsasouza <wsasouza@hotmail.com>
2022-03-01 23:06:02 -0300

    Fix: Backend Recover

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -1,4 +1,41 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+
 const Feed = () => {
+
+  const [loading, setLoading] = useState(false);  
+  const [pins, setPins] = useState(null);
+
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+
+    if(categoryId) {
+      const query = searchQuery(categoryId);
+
+      client.fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+    } else {
+       client.fetch(feedQuery)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+      }
+  }, [categoryId]); 
+
+  if(loading) return <Spinner message="Estamos adicionando novas ideias ao seu feed!"/>
+
   return (
     <>
      <h1>Eu sou a Feed</h1>
---
+++ shareme_frontend/src/components/MasonryLayout.jsx
@@ -0,0 +1,10 @@
+const MasonryLayout = () => {
+  return (
+    <>
+     <h1>Eu sou a MasonryLayout</h1>
+     <p>Wartinho</p>
+    </>
+  )
+}
+
+export default MasonryLayout;
